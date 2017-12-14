60b4f795babc0e24f31bdaa56cf21b64077cc796
balia <balia.yassin90@gmail.com>
2017-12-14 14:02:58 +0200

    C:\Users\User\Documents\Aptana Studio 3 Workspace update layout3

---
+++ Documents/Aptana Studio 3 Workspace/Tar6/includes/js/main.js
@@ -0,0 +1,27 @@
+
+var person={
+	
+	firstName:'balia',
+	lastName:'yassin',
+	kids:0,
+	address:{
+		con:'isreal',
+		city:'shfaram',
+		street:'fgdf',
+		postcode:2020000,
+	}
+	getFullName:function () {return this.firstName + " " + this.lastName},
+	hasKids:function (){ return ; } 
+	  
+	  	
+	  	
+	  
+	
+   
+
+
+
+
+
+
+
---
+++ Documents/Aptana Studio 3 Workspace/Tar7/includes/js/main.js
@@ -0,0 +1,25 @@
+ $("document").ready(function() {
+ console.log("hello world using JQuery!");
+ });
+ 
+
+
+$(document).ready( function ()){
+	
+	$(`main`).append{
+		
+		`<h1> balia yassin</h1>
+		 <img src="images/img2.png" />
+		`
+	}
+	
+	
+var img=$('<img>').attr( 'src','images/img2.png')
+var title=$('<h1>').text("hello world")
+var button=$('<button>').attr('id','switch').text('change title!')	
+	
+	$('main')
+	
+}
+
+
