40b1ecbaef7e267027bb52a79db079ee3740efa5
balia <balia.yassin90@gmail.com>
2018-04-23 19:19:01 +0300

    first commit

---
+++ AndroidStudioProjects/MyGame/app/src/main/java/com/shenkar/balia/mygame/MainActivity.java
@@ -0,0 +1,13 @@
+package com.shenkar.balia.mygame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
